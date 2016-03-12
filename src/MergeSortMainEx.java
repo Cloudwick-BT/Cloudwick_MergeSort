d697bbb6fd51526c61bc301fb5515896c7221036
bhavintandel <bhautandel@yahoo.com.com>
2016-03-12 17:13:23 +0000

    refactored the program, it extends MergeSorting class

--- src/MergeSortMainEx.java
+++ src/MergeSortMainEx.java
@@ -1,67 +1,11 @@
-import java.util.*;
-
 /**
  * 
  * @author Bhavin Tandel
  * @since 12/03/2016; 13:15
  */
 
-public class MergeSortMainEx {
-	
-	public static int[] merge(int[] leftArray, int[] rigthArray, int[] finalArray){
-		int lLength = leftArray.length;
-		int rLength = rigthArray.length;
-		int i = 0; // to trace left array
-		int j = 0; // to trace right array
-		int k = 0; // to trace main array;
+public class MergeSortMainEx extends MergeSorting {
 		
-		while(i < lLength && j <rLength){
-			if (leftArray[i] < rigthArray[j]){
-				finalArray[k] = leftArray[i];
-				i++;
-			}
-			else{
-				finalArray[k] = rigthArray[j];
-				j++;
-			}
-			k++;
-		}
-		
-		while (i < lLength){
-			finalArray[k] = leftArray[i];
-			i++;
-			k++;
-		}
-		
-		while (j < rLength){
-			finalArray[k] = rigthArray[j];
-			j++;
-			k++;
-		}
-		leftArray = null;
-		rigthArray = null;
-		return finalArray;
-	}
-	
-	
-	public static int[] mergeSort(int[] arr){
-		int mid = arr.length/2;	
-		int[] leftArray = Arrays.copyOfRange(arr, 0, mid);
-		int[] rigthArray = Arrays.copyOfRange(arr, mid, arr.length);
-		
-		if (mid < 1){
-			return arr;
-		}
-		else{
-			leftArray = mergeSort(leftArray);
-			rigthArray = mergeSort(rigthArray);
-			arr = merge(leftArray, rigthArray, arr);
-		}
-		
-		return arr;
-	}
-	
-	
 	public static void main(String[] args){
 		int[] arr = {8,9,10,2,1,5,3,5,3,2,3};
 		
