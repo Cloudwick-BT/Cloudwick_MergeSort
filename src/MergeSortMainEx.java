4a5004125fa08a027ed0c631d718f53993ef3e50
bhavintandel <bhautandel@yahoo.com.com>
2016-03-14 10:19:42 +0000

    Added random number generator with tie calculation added

--- src/MergeSortMainEx.java
+++ src/MergeSortMainEx.java
@@ -1,19 +1,33 @@
+import java.util.Random;
+
 /**
  * 
  * @author Bhavin Tandel
  * @since 12/03/2016; 13:15
  */
 
+
 public class MergeSortMainEx extends MergeSorting {
 		
 	public static void main(String[] args){
-		int[] arr = {8,9,10,2,1,5,3,5,3,2,3};
+		int length = 1000000;
+		int[] arr = new int[length];
+		Random rnd = new Random();
 		
+		for(int i = 0; i < length; i++){
+			arr[i] = rnd.nextInt(Integer.MAX_VALUE);
+		}
+		long startTime = System.currentTimeMillis();
 		arr = mergeSort(arr);
+		long stopTime = System.currentTimeMillis();
+		
+		
 	
 		for(int i = 0; i < arr.length; i++){
 			System.out.println(arr[i]);
 		}
+		
+		System.out.println("Total run time : " +( stopTime - startTime));
 
 	}
 
